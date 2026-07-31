4840b77450bc56e96f5f70687f888fb1d925d12f
Hyder Hadi <hyder@hyderhadi.xyz>
2026-07-31 16:03:36 +0300

    Worked on the scroll indicator and scrollling

--- app/src/main/java/xyz/hyderhadi/personaljournal/ui/entryscreen/JournalScreen.kt
+++ app/src/main/java/xyz/hyderhadi/personaljournal/ui/entryscreen/JournalScreen.kt
@@ -1,25 +1,31 @@
 package xyz.hyderhadi.personaljournal.ui.entryscreen
 
-import android.R
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import xyz.hyderhadi.personaljournal.ui.theme.AppTypography
+import xyz.hyderhadi.personaljournal.ui.theme.Shapes
 
 
 @Composable
@@ -28,9 +34,9 @@ fun JournalEntryScreen(
     modifier: Modifier = Modifier
 ) {
     // for initial testing of the entryScreen
-    var text by rememberSaveable { mutableStateOf("") }
-
+    val state = rememberTextFieldState()
     val colors = MaterialTheme.colorScheme
+    val scrollState = rememberScrollState()
 
     Box(
         modifier = modifier.statusBarsPadding()
@@ -38,34 +44,60 @@ fun JournalEntryScreen(
         Column {
 
             Box(
-                modifier = Modifier.fillMaxSize().background(colors.secondaryContainer),
-
+                modifier = Modifier.fillMaxSize().background(colors.secondaryContainer)
             ) {
 
                 BasicTextField(
-                    value = text,
-                    onValueChange = { text = it },
                     modifier = Modifier
-                        .fillMaxSize()
-                        .padding(24.dp),
+                        .fillMaxWidth()
+                        .padding(horizontal = 24.dp, vertical = 12.dp)
+                        .verticalScroll(scrollState)
+                        .imePadding(),
                     textStyle = AppTypography.bodyLarge.copy(
                         color =  colors.onPrimary
                     ),
+                    state = state,
                     cursorBrush = SolidColor(colors.onPrimaryContainer),
-                    decorationBox = { innerTextField ->
+                    decorator = { innerTextField ->
                         Box(
                             modifier = Modifier.fillMaxSize()
                         ) {
-                            if(text.isEmpty()) {
+                            if(state.text.isEmpty()) {
                                 Text("Type Your Entry ...",
                                     color = colors.onSecondary
-                                    )
+                                )
                             }
 
                             innerTextField()
                         }
                     }
                 )
+
+                if(scrollState.maxValue > 0) {
+                    Box(
+                        modifier = Modifier
+                            .align(Alignment.CenterEnd)
+                            .fillMaxHeight()
+                            .width(3.dp)
+                            .background(colors.surfaceContainerLowest.copy(alpha = 0.3f))
+                    )
+
+                    val progress =
+                        scrollState.value.toFloat() / scrollState.maxValue.toFloat()
+
+                    Box(
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .offset(y = (progress * 300).dp)
+                            .width(6.dp)
+                            .height(48.dp)
+                            .background(
+                                colors.surfaceDim,
+                                Shapes.small
+                            )
+                    )
+                }
+
             }
         }
 
